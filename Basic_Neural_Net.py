e8cc4ab57de048af22cdc1b95ca3ecf3d954f5ae
argupta98 <argupta@mit.edu>
2017-09-17 23:14:25 -0400

    Added Accuracy functionality

--- Basic_Neural_Net.py
+++ Basic_Neural_Net.py
@@ -11,17 +11,19 @@ http://www.wildml.com/2015/09/implementing-a-neural-network-from-scratch/
 """
 
 #Import Packages
+
 #import numpy for fast matrix computations
 import numpy as np
+
 #import sklearn for datasets
 import sklearn.datasets
+
 #import matplotlib for graphing utility
 import matplotlib.pyplot as plt
+
 #import to get a normal linear model for comparison
 import sklearn.linear_model
 
-#import matplotlib
-#import sklearn
 
 """
 Generate dataset for testing. sklearn has a ton of great datasets that are easy 
@@ -38,8 +40,7 @@ Train a out-of-the-box linear classifier as a baseline for comparison
 linear_classifier=sklearn.linear_model.LogisticRegressionCV()
 linear_classifier.fit(X,y) 
 
-
-#This function is copied directly from the tutorial
+#function for plotting decision function
 def plot_decision_boundary(pred_func): 
     # Set min and max values and give it some padding 
     x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5 
@@ -58,7 +59,7 @@ def plot_decision_boundary(pred_func):
 plot_decision_boundary(lambda x: linear_classifier.predict(x))
 
 """
-Setup functions to Train Neural Net
+Setup functions to Train and Test Neural Net
 """
 #NN structural parameters
 # 2 dimentional points, so input layer should be 2 dimentional
@@ -70,7 +71,7 @@ epsilon = 0.01
 #Controls how much we are regularizing per iteration
 reg_lambda = .01
 
-def train_nn(hidden_dim, data=X, labels=y, iterations=400):
+def train_nn(hidden_dim, data=X, labels=y, iterations=2000):
     
     #initialize with random weights
     np.random.seed(0)
@@ -87,31 +88,31 @@ def train_nn(hidden_dim, data=X, labels=y, iterations=400):
     for i_train in range(iterations):
         
         #compute current prediction (forward propagation)
-        out_layer_1 = data.dot(Theta_1)+b_1
-        out_layer_1 = np.tanh(out_layer_1)
-        out_layer_2 = out_layer_1.dot(Theta_2)+b_2
-        out_final = np.exp(out_layer_2)
-        probs = out_final / np.sum(out_final, axis=1, keepdims=True)
+        out_layer_1=data.dot(Theta_1)+b_1
+        out_layer_1=np.tanh(out_layer_1)
+        out_layer_2=out_layer_1.dot(Theta_2)+b_2
+        out_final=np.exp(out_layer_2)
+        probs=out_final / np.sum(out_final, axis=1, keepdims=True)
         
         #compute gradients for gradient descent (backpropagation)
         #start at end node, work backwards starting at third layer
-        delta_3 = probs
-        delta_3[range(len(data)), labels] -= 1
-        dT_2 = (out_layer_1.T).dot(delta_3)
-        db_2 = np.sum(delta_3, axis=0, keepdims=True)
-        delta_2 = delta_3.dot(Theta_2.T)*(1-np.power(out_layer_1, 2))
-        dT_1 = np.dot(data.T, delta_2)
-        db_1 = np.sum(delta_2, axis=0)
+        delta_3=probs
+        delta_3[range(len(data)), labels]-=1
+        dT_2=(out_layer_1.T).dot(delta_3)
+        db_2=np.sum(delta_3, axis=0, keepdims=True)
+        delta_2=delta_3.dot(Theta_2.T)*(1-np.power(out_layer_1, 2))
+        dT_1=np.dot(data.T, delta_2)
+        db_1=np.sum(delta_2, axis=0)
         
         #regularize terms
-        dT_1 += reg_lambda*Theta_1
-        dT_2 += reg_lambda*Theta_2
+        dT_1+=reg_lambda*Theta_1
+        dT_2+=reg_lambda*Theta_2
         
         #update weights from gradient descent
-        Theta_1 += -epsilon*dT_1
-        Theta_2 += -epsilon*dT_2
-        b_1 += -epsilon*db_1
-        b_2 += -epsilon*db_2
+        Theta_1+=-epsilon*dT_1
+        Theta_2+=-epsilon*dT_2
+        b_1+=-epsilon*db_1
+        b_2+=-epsilon*db_2
         
     return {'Theta_1':Theta_1, 'Theta_2': Theta_2, 'b_1':b_1, 'b_2':b_2}
 
@@ -124,12 +125,17 @@ def predict(model, data):
     out_layer_1=np.tanh(out_layer_1)
     out_layer_2=out_layer_1.dot(Theta_2)+b_2
     out_final=np.exp(out_layer_2)
-    probs= out_final / np.sum(out_final, axis=1, keepdims=True)
-    return np.argmax(probs, axis=1)
+    return out_final / np.sum(out_final, axis=1, keepdims=True) 
     
+def test(model, data, labels):
+    probs=predict(model, data) 
+    log_probs=-np.log(probs[range(len(data)),labels])
+    error=np.sum(log_probs)
+    return 1-(1/len(data))*error
+
 """
 Train and Test Model
 """
-model=train_nn(3)
-
-plot_decision_boundary(lambda x: predict(model, x))
+model=train_nn(3, iterations=20000)
+plot_decision_boundary(lambda x: np.argmax(predict(model, x), axis=1))
+print("Accuracy: ",test(model,X,y))
